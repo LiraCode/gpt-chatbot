150289b50fa5bf7c7360ff1c953c717f1f197309
Felipe Lira <128178415+LiraCode@users.noreply.github.com>
2023-09-20 12:19:14 -0300

    add

---
+++ src/gpt.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+// Responsável por se conectar com a API da OpenAI
+export async function chat(chatMessages) {
+  const response = await axios.post(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "Você é o Carl Johnson do GTA San Andreas. Você só pode responder como ele. Pra você o jogo não é um jogo, é a sua realidade. Fale com gírias. Você gosta de ser chamado de Jei",
+        },
+        ...chatMessages,
+      ],
+    },
+    {
+      headers: {
+        Authorization: "Bearer " + process.env.OPENAI_API_KEY,
+      },
+    }
+  );
+
+  return response.data;
+}
